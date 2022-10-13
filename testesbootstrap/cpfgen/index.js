1629b3d691648470da9cbaac22881cc4e7e0aca9
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-10-13 14:05:24 -0300

    ajustes no javascript

--- testesbootstrap/cpfgen/index.js
+++ testesbootstrap/cpfgen/index.js
@@ -2,15 +2,10 @@
 const btn = document.querySelector("#buton")
 btn.addEventListener("click" , function (e){
 e.preventDefault()
-
     const gerarCpf = () => {
         let newCpf = '';
-        //uso no primeiro digito verificador
         let x = 0;
         let j = 0;
-        //uso no segundo digito verificador
-        let c = 0;
-        let b = 1;
      
             for(let i = 0; i<9; i++){
               newCpf += (Math.floor(Math.random() * (10 - 0)));
@@ -27,66 +22,24 @@ e.preventDefault()
       } else {
         newCpf += 11 - (x % 11);
       }   
-    
+    x = 0;
+    j = 0;
     //segundo digito verificador
       for(let i = 10; i>=2; i--){
-        c += newCpf.charAt(b)*i;
-        b++;
+        x += newCpf.charAt(j)*i;
+        j++;
       }
     
-      if(c % 11 === 0 || c % 11 === 1){
+      if(x % 11 === 0 || x % 11 === 1){
         newCpf += 0;
       }else{
-        newCpf += 11 - (c % 11);
+        newCpf += 11 - (x % 11);
       }
     
       const result = document.querySelector("#CpfReceiver")
-      result.value = innerText = newCpf;
+      result.value = innerText = newCpf;    
       console.log(newCpf);
-    
     } 
-
     gerarCpf()
-
 })
 
-/* const gerarCpf = () => {
-    let newCpf = '';
-    //uso no primeiro digito verificador
-    let x = 0;
-    let j = 0;
-    //uso no segundo digito verificador
-    let c = 0;
-    let b = 1;
- 
-        for(let i = 0; i<9; i++){
-          newCpf += (Math.floor(Math.random() * (10 - 0)));
-    }  
-
-  //primeiro digito verificador ->
-  for(let i=10; i>=2; i--){
-    x += newCpf.charAt(j)*i;
-    j++;
-  }
-  
-     if(x % 11 === 1 || x % 11 === 0 ){
-    newCpf += 0;
-  } else {
-    newCpf += 11 - (x % 11)
-  }   
-
-//segundo digito verificador
-  for(let i = 10; i>=2; i--){
-    c += newCpf.charAt(b)*i;
-    b++;
-  }
-
-  if(c % 11 === 0 || c % 11 === 1){
-    newCpf += 0;
-  }else{
-    newCpf += 11 - (c % 11)
-  }
-
-  console.log(newCpf);
-
-}  */
